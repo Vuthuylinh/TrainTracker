66eb64395bfab0d6bcfdfc060eeda5a4076ed6fd
Linh Vu <thuylinh2490@gmail.com>
2021-03-18 22:24:15 -0400

    server(db): create model train

---
+++ server/db/model/train.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Train = db.define('train',{
+  name:{
+    type:Sequelize.STRING,
+    unique:true,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue: "https://image.shutterstock.com/image-vector/train-260nw-104147417.jpg"
+  },
+  arrivalTime1:{
+    type: Sequelize.DATE
+  },
+  arrivalTime2:{
+    type: Sequelize.DATE
+  }
+})
+
+module.exports = Train
