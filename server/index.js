a8cc33a998f1e27110fced22824ca7cf9b3c2a31
Linh Vu <thuylinh2490@gmail.com>
2021-03-18 22:14:19 -0400

    server(index):setup server to start

--- server/index.js
+++ server/index.js
@@ -3,29 +3,49 @@ const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser');
 const app = express()
+const PORT = process.env.PORT || 8080
 
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
 
-// logging middleware
-app.use(morgan('dev'))
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
 
-// static file-serving middleware
-app.use(express.static(path.join(__dirname, '..', 'public')))
+  // body parsing middleware
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-// body parsing middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+  // api route
+  app.use('/api', require('./api'))
 
-// api route
-app.use('/api', require('./api'))
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
 
- // sends index.html
- app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-})
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  app.listen(PORT, () =>
+    console.log(`App is running on port ${PORT}`)
+  )
+}
 
-// error handling endware
-app.use((err, req, res, next) => {
-  console.error(err)
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
+async function bootApp() {
+  await createApp()
+  await startListening()
+}
+
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
+module.exports = app
