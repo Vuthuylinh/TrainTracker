88079637f418ad2f6e15a193fed3b3f2962cb925
Linh Vu <thuylinh2490@gmail.com>
2021-03-18 21:57:44 -0400

    static middleware

--- server/index.js
+++ server/index.js
@@ -4,3 +4,7 @@ const app = express()
 const morgan = require('morgan')
 // logging middleware
 app.use(morgan('dev'))
+
+// static file-serving middleware
+const path = require('path')
+app.use(express.static(path.join(__dirname, '..', 'public')))
