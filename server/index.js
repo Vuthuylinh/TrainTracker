818e639fa8d7d7d5878025f1383c2052a7264f1c
Linh Vu <thuylinh2490@gmail.com>
2021-03-18 21:59:20 -0400

    server(index): setup parsing middleware

--- server/index.js
+++ server/index.js
@@ -8,3 +8,7 @@ app.use(morgan('dev'))
 // static file-serving middleware
 const path = require('path')
 app.use(express.static(path.join(__dirname, '..', 'public')))
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
